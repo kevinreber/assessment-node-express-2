46b3ebf73690e973ac2b7e5bac6b84897858b6ee
kevinreber <kevinreber1@gmail.com>
2020-06-16 00:05:16 -0700

    step 2: added tests

--- step 2/timeWord.test.js
+++ step 2/timeWord.test.js
@@ -1,7 +1,65 @@
 const timeWord = require('./timeWord');
 
+
+// Input	  Expected Output
+// 00:00	  midnight
+// 00:12	  twelve twelve am
+// 01:00	  one o’clock am
+// 06:01	  six oh one am
+// 06:10	  six ten am
+// 06:18	  six eighteen am
+// 06:30	  six thirty am
+// 10:34	  ten thirty four am
+// 12:00	  noon
+// 12:09	  twelve oh nine pm
+// 23:23	  eleven twenty three pm
+
 describe('#timeword', () => {
   test('it is a function', () => {
     expect(typeof timeWord).toBe('function');
+  });
+
+  test('00:00 should be midnight', () => {
+    expect(timeWord('00:00')).toBe('midnight');
+  });
+
+  test('00:12 should be twelve twelve am', () => {
+    expect(timeWord('00:12')).toBe('twelve twelve am');
+  });
+
+  test('01:00 should be one o’clock am', () => {
+    expect(timeWord('01:00')).toBe("one o'clock am");
+  });
+
+  test('06:01 should be six oh one am', () => {
+    expect(timeWord('06:01')).toBe("six oh one am");
+  });
+
+  test('06:10 should be six ten am', () => {
+    expect(timeWord('06:10')).toBe("six ten am");
+  });
+
+  test('06:18 should be six eighteen am', () => {
+    expect(timeWord('06:18')).toBe("six eighteen am");
+  });
+
+  test('06:30 should be six thirty am', () => {
+    expect(timeWord('06:30')).toBe("six thirty am");
+  });
+
+  test('10:34 should be ten thirty four am', () => {
+    expect(timeWord('10:34')).toBe("ten thirty four am");
+  });
+
+  test('12:00 should be noon', () => {
+    expect(timeWord('12:00')).toBe("noon");
+  });
+
+  test('12:09 should be twelve oh nine pm', () => {
+    expect(timeWord('12:09')).toBe("twelve oh nine pm");
+  });
+
+  test('23:23 should be eleven twenty three pm', () => {
+    expect(timeWord('23:23')).toBe("eleven twenty three pm");
   });
 });
